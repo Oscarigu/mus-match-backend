4f47d01610b4e44ecb6814ee1eeeff07f3f34c14
oscarigu <38469752+Oscarigu@users.noreply.github.com>
2025-10-29 12:42:25 +0100

    feat: add health endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
 
 // Root route
 router.get("/", (req, res) => {
   res.json("All good in here");
 });
+
+// GET /health 
+router.get('/health', (req, res) => {
+  // send ping to prevent inactivity on mongodb atlas
+  mongoose.connection.db.admin().ping()
+    .then( () => {
+      res.status(200).json({
+        status: 'ok',
+        timestamp: new Date().toISOString()
+      });
+    })
+    .catch(err => {
+      console.error('MongoDB ping failed:', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to connect to MongoDB',
+      });
+    });
+});
+
 
 // Import resource routes
 const gameRoutes = require("./game.routes");
